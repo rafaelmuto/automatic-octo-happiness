9d9be6e56e383f0ef9b51e611c46c0f58e77f2f2
rafaelmuto <r.nagahama@gmail.com>
2025-07-13 23:28:53 -0300

    index by author

--- DjangoProject/library/urls.py
+++ DjangoProject/library/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "library"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('author/<int:author_id>/', views.IndexByAuthorView.as_view(), name='index_by_author'),
 ]
--- DjangoProject/library/views.py
+++ DjangoProject/library/views.py
@@ -12,3 +12,14 @@ class IndexView(generic.ListView):
         Return the list of books.
         """
         return Book.objects.all()
+
+class IndexByAuthorView(generic.ListView):
+    model = Book
+    context_object_name = "book_list"
+
+    def get_queryset(self):
+        """
+        Return the list of books by a specific author.
+        """
+        author_id = self.kwargs.get("author_id")
+        return Book.objects.filter(author_id=author_id)
