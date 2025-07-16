3fb29533747957b76bb98197610830536199ef7c
rafaelmuto <r.nagahama@gmail.com>
2025-07-15 23:44:02 -0300

    feat: Reposition edit links in list views and format URLs
    Moved the '[Edit]' links to the far right of each entry in the book and
    author list views ( and ) for better
    visual alignment. Also applied minor formatting adjustments to .

--- DjangoProject/library/urls.py
+++ DjangoProject/library/urls.py
@@ -7,7 +7,11 @@ app_name = "library"
 urlpatterns = [
     path("", views.BookListView.as_view(), name="index"),
     path("books/<int:pk>/", views.BookDetail.as_view(), name="book_detail"),
-    path("author/<int:author_id>/", views.BookListByAuthorView.as_view(), name="index_by_author"),
+    path(
+        "author/<int:author_id>/",
+        views.BookListByAuthorView.as_view(),
+        name="index_by_author",
+    ),
     path("authors/", views.AuthorList.as_view(), name="author_list"),
     path("authors/<int:pk>/", views.AuthorDetail.as_view(), name="author_detail"),
     path("authors/add/", views.AuthorCreate.as_view(), name="author_add"),
